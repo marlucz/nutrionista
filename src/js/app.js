6eeacf2d720182c121e9039eb15d3489f3b2fcdd
Bukacz <mbukacz@gmail.com>
2019-05-28 23:06:01 +0200

    App non-responsive interface done

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 import '../scss/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnShopping = document.querySelector('[data-nav="shopping-cart"]');
+    const btnFavourites = document.querySelector('[data-nav="favourites"]');
+    const shoppingList = document.querySelector('.shopping');
+    const favouritesList = document.querySelector('.likes');
+    btnShopping.addEventListener('click', () => {
+        shoppingList.classList.toggle('active');
+    });
+    btnFavourites.addEventListener('click', () => {
+        favouritesList.classList.toggle('active');
+    });
+
+});
